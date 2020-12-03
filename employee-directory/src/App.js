a953a3b01f587fed77572e2d380cba40e5d394a8
Jon Jessee <tjessee7624@gmail.com>
2020-12-02 21:25:46 -0700

    Altered Table component and App so that employee data is now passed in as props.

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,14 +2,35 @@ import React from 'react';
 import Header from './Components/Header';
 import Table from './Components/Table';
 import Button from './Components/Button';
+import employees from './employees.json';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      employees
+    };
+  };
+
   render() {
     return (
       <div>
         <Header/>
         <Button/>
-        <Table/>
+        {this.state.employees.map(employee => {
+          return (
+            <Table
+              key={employee.id}
+              id={employee.id}
+              firstName={employee.firstName}
+              lastName={employee.lastName}
+              title={employee.title}
+              managerFirst={employee.managerFirst}
+              managerLast={employee.managerLast}
+            />
+          );
+        })};
       </div>
     );
   };
--- employee-directory/src/Components/Table.jsx
+++ employee-directory/src/Components/Table.jsx
@@ -1,45 +1,31 @@
 import React from 'react';
-import jsonData from '../employees.json';
 
-class Table extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            employees: []
-        };
-    };
-
-    render() {
-        return (
-            <table className="table table-striped">
-                <thead className="thead-dark">
-                    <tr>
-                        <th scope="col">Employee ID</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Manager First Name</th>
-                        <th scope="col">Manager Last Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {jsonData.map(employee => {
-                        return (
-                            <tr> 
-                                <td key={employee.id}>{employee.id}</td>
-                                <td key={employee.id}>{employee.firstName}</td>
-                                <td key={employee.id}>{employee.lastName}</td>
-                                <td key={employee.id}>{employee.title}</td>
-                                <td key={employee.id}>{employee.managerFirst}</td>
-                                <td key={employee.id}>{employee.managerLast}</td>
-                            </tr>
-                        );
-                    })};
-                </tbody>  
-            </table>
-        );
-    };
+function Table(props) {
+    return (
+        <table className="table table-striped">
+            <thead className="thead-dark">
+                <tr>
+                    <th scope="col">Employee ID</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Manager First Name</th>
+                    <th scope="col">Manager Last Name</th>
+                </tr>
+            </thead>
+            <tbody>  
+                <tr> 
+                    <td key={props.id}>{props.id}</td>
+                    <td key={props.id}>{props.firstName}</td>
+                    <td key={props.id}>{props.lastName}</td>
+                    <td key={props.id}>{props.title}</td>
+                    <td key={props.id}>{props.managerFirst}</td>
+                    <td key={props.id}>{props.managerLast}</td>
+                </tr>
+            </tbody>  
+        </table>
+    );
 };
+
 
 export default Table;
